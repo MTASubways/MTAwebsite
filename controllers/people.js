3af5da4e59e17568c234128e82c523d891dfcd7f
Trevor Preston <trevorpreston4@gmail.com>
2016-08-04 13:43:12 -0400

    dummy graph

--- controllers/people.js
+++ controllers/people.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 router.get('/', function(req, res){
-  res.render('index')
+  res.render('index', {funData: [65, 59, 80, 81, 56, 55, 40]})
 })
 
 module.exports= router
