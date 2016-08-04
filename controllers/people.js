b87e348f9fee93febaf185b0b7e041fa8f9eb500
Trevor Preston <trevorpreston4@gmail.com>
2016-08-04 13:45:54 -0400

    fixed small syntax error

--- controllers/people.js
+++ controllers/people.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 
 router.get('/', function(req, res){
   res.render('index', {funData: [65, 59, 80, 81, 56, 55, 40]})
-
+}
 // router.get('/', peopleModel.people, function(req, res){
 //   res.render('/index', {in: res.in, out : res.out})
 
